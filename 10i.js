89c7b93a16ddb1f1f0533dfdcfaa2e87a2024184
khudobakhshzn-lab <khudobakhsh.zn@gmail.com>
2026-06-26 18:03:35 +0500

    Update 10i.js

--- 10i.js
+++ 10i.js
@@ -1,13 +1,10 @@
 let calculation = localStorage.getItem('calculation') || '';
 
-      // Display the calculation when the page first loads.
       displayCalculation();
 
       function updateCalculation(value) {
         calculation += value;
 
-        // Display the calculation on the page
-        // instead of console.log
         displayCalculation();
 
         localStorage.setItem('calculation', calculation);
